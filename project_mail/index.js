27cfce8a3d56ada709c5f1c15bc9f52420b2b82d
Ravi Teja <rpillala@iu.edu>
2024-04-27 19:22:02 +0000

    main

--- project_mail/index.js
+++ project_mail/index.js
@@ -1,8 +1,7 @@
 const { Mailsending } = require('./1.js');
-const { WriteToBigQuery } = require('./6.js');
+const { WriteToBigQuery } = require('./2.js');
 
 exports.Main = (message, context) => {
     Mailsending(message, context);
     WriteToBigQuery(message, context);
 }
-
