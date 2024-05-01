325e0414bebcd24895d354d2762d1c9f34c699e8
Ravi Teja <rpillala@iu.edu>
2024-05-01 04:09:31 +0000

    Main

--- project_mail/1.js
+++ project_mail/1.js
@@ -9,51 +9,34 @@ const storage = new Storage();
 
 exports.Mailsending = async (message, context) => {
     try {
-        // Define the bucket names
+
         const initialBucketName = 'gs://initialbucket_2';
         const reportBucketName = 'gs://vulnerability_report';
-
-        // Define the constant filename in the report bucket
         const reportFileName = 'vulnerability_report.csv';
 
-        // Function to copy file from initial bucket to report bucket
         async function copyFileAndDelete(sourceBucketName, sourceFilename, destBucketName, destFilename) {
-            // Copy the file to the report bucket
             await storage.bucket(sourceBucketName).file(sourceFilename).copy(storage.bucket(destBucketName).file(destFilename));
-
-            // Delete the file from the initial bucket
             await storage.bucket(sourceBucketName).file(sourceFilename).delete();
-            
             console.log(`File ${sourceFilename} copied to ${destBucketName} as ${destFilename} and deleted from ${sourceBucketName}.`);
         }
-
-        // Get a list of files in initial bucket
         const [initialFiles] = await storage.bucket(initialBucketName).getFiles();
-
-        // Loop through initial bucket files and copy them to report bucket
         for (const file of initialFiles) {
             const sourceFilename = file.name;
-            // Copy only if the file is not already named as reportFileName
             if (sourceFilename !== reportFileName) {
                 await copyFileAndDelete(initialBucketName, sourceFilename, reportBucketName, reportFileName);
             }
         }
 
-        // Set SendGrid API key
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-        // Parse message data
         const reportData = JSON.parse(Buffer.from(message.data, 'base64').toString());
 
-        // Initialize Firestore
         const firestore = new Firestore();
 
-        // Query Firestore for subscriber emails based on asset name
         const usersSnapshot = await firestore.collection('Vulnerability_subscribers').where('assetName', '==', reportData.assetName).get();
         const subscriberEmails = usersSnapshot.docs.map(doc => doc.data().email_address);
         const subscriberDocs = usersSnapshot.docs.map(doc => doc.ref);
 
-        // Retrieve file from report bucket
         const bucketName = reportBucketName;
         const [files] = await storage.bucket(bucketName).getFiles();
 
@@ -65,12 +48,9 @@ exports.Mailsending = async (message, context) => {
         
         const fileName = files[0].name;
         const file = storage.bucket(bucketName).file(fileName);
-
-        // Arrays to store vulnerability data
         const activeNewVulnerabilities = [];
         const fixedVulnerabilities = [];
 
-        // Read CSV file and process data
         await new Promise((resolve, reject) => {
             file.createReadStream()
                 .pipe(csv())
@@ -95,31 +75,25 @@ exports.Mailsending = async (message, context) => {
                 });
         });
 
-        // If no vulnerabilities found, log and return
         if (activeNewVulnerabilities.length === 0 && fixedVulnerabilities.length === 0) {
             console.warn('No data found in CSV file for asset: ', reportData.assetName);
             return;
         }
 
-        // Format CSV data
         const activeNewCSVData = formatCSVData(activeNewVulnerabilities);
         const fixedCSVData = formatCSVData(fixedVulnerabilities);
 
-        // Write CSV data to temporary files
         const activeNewCSVFilePath = path.join('/tmp', 'active_new_vulnerability_report.csv');
         const fixedCSVFilePath = path.join('/tmp', 'fixed_vulnerability_report.csv');
 
         fs.writeFileSync(activeNewCSVFilePath, activeNewCSVData);
         fs.writeFileSync(fixedCSVFilePath, fixedCSVData);
 
-        // Read CSV data from temporary files
         const filteredactiveNewCSV = fs.readFileSync(activeNewCSVFilePath, 'utf8');
         console.log('Filtered active new CSV data:', filteredactiveNewCSV);
-
         const filteredfixedCSV = fs.readFileSync(fixedCSVFilePath, 'utf8');
         console.log('Filtered fixed CSV data:', filteredfixedCSV);
 
-        // Send emails with attachments to subscribers
         subscriberEmails.forEach(async (email) => {
             const senderEmail = process.env.SENDGRID_SENDER;
             const senderUsername = email.split('@')[0];
--- project_mail/2.js
+++ project_mail/2.js
@@ -4,40 +4,30 @@ const csv = require('csv-parser');
 
 exports.WriteToBigQuery = async (message, context) => {
     try {
-        // Initialize BigQuery client
-        const bigquery = new BigQuery();
 
-        // Define BigQuery dataset and table
+        const bigquery = new BigQuery();
         const datasetId = 'vulnerability_report';
         const tableId = 'Reporting';
-
-        // Initialize Storage client
         const storage = new Storage();
-
-        // Define bucket name
-        const bucketName = 'vulnerability_report'; // Remove gs:// prefix
-
-        // Read data from CSV files in the bucket
+        const bucketName = 'gs://vulnerability_report';
         const [files] = await storage.bucket(bucketName).getFiles();
-
-        // Object to store vulnerability counts for each asset
         const assetVulnerabilityCounts = {};
 
         for (const file of files) {
             const fileData = await file.createReadStream().pipe(csv()).toArray();
             fileData.forEach(row => {
                 const assetName = row['DNS'];
-                const ipAddress = row['IP']; // Extract IP address from the report
+                const ipAddress = row['IP'];
                 if (!assetVulnerabilityCounts[assetName]) {
                     assetVulnerabilityCounts[assetName] = {
                         NumActive: 0,
                         NumNew: 0,
                         NumFixed: 0,
-                        IpAddresses: new Set(), // Store unique IP addresses for each asset
+                        IpAddresses: new Set(),
                     };
                 }
 
-                if (ipAddress) { // Check if IP address is not null
+                if (ipAddress) {
                     assetVulnerabilityCounts[assetName].IpAddresses.add(ipAddress);
                 }
 
@@ -51,19 +41,11 @@ exports.WriteToBigQuery = async (message, context) => {
                 }
             });
         }
-
-        // Delete existing Reporting table if it exists
         await deleteExistingTable(bigquery, datasetId, tableId);
-
-        // Create Reporting table
         await createReportingTable(bigquery, datasetId, tableId);
-
-        // Insert new data into the reporting table after a delay
         await new Promise(resolve => setTimeout(resolve, 5000)); // Adjust delay as needed
         await insertDataIntoReportingTable(bigquery, datasetId, tableId, assetVulnerabilityCounts);
-
         console.log('Data successfully inserted into BigQuery.');
-
     } catch (error) {
         console.error('Error writing data to BigQuery:', error);
     }
@@ -74,17 +56,14 @@ async function deleteExistingTable(bigquery, datasetId, tableId) {
         const dataset = bigquery.dataset(datasetId);
         const table = dataset.table(tableId);
 
-        // Check if the table exists
         const [exists] = await table.exists();
         if (exists) {
-            // If the table exists, delete it
             await table.delete();
             console.log('Existing table deleted successfully.');
         } else {
             console.log('Table does not exist. Skipping deletion.');
         }
     } catch (error) {
-        // Log the error without throwing
         console.error(`Error deleting existing table: ${error}`);
     }
 }
@@ -103,13 +82,11 @@ async function createReportingTable(bigquery, datasetId, tableId) {
                     { name: 'NumRemediated', type: 'INTEGER' },
                 ],
             },
-            // Specify 'exists' option to not throw an error if the table already exists
             exists: 'skip'
         });
 
         console.log(`Table ${tableId} created successfully.`);
     } catch (error) {
-        // Ignore the error if the table already exists
         if (!error.message.includes('Already Exists:')) {
             throw new Error(`Error creating reporting table: ${error}`);
         }
@@ -120,8 +97,6 @@ async function insertDataIntoReportingTable(bigquery, datasetId, tableId, assetV
     try {
         const dataset = bigquery.dataset(datasetId);
         const table = dataset.table(tableId);
-
-        // Insert new data into the reporting table
         const rowsToInsert = [];
         for (const assetName in assetVulnerabilityCounts) {
             const { NumActive, NumNew, NumFixed, IpAddresses } = assetVulnerabilityCounts[assetName];
